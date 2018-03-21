195d2a345191ffa0a7648f379e47fa81f6dbd83e
logonrm <logonrm@fiap.com.br>
2018-03-21 11:13:56 -0300

    Aula - 21/03

---
+++ Assets/Scripts/IfEncadeado.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IfEncadeado : MonoBehaviour {
+    public Button gasolina, alcool;
+    public string combustivel;
+    double preco;
+    public  int qtde;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        if (combustivel == "G")
+        {
+            if(qtde <=20)
+            {
+                preco = 3;
+            }
+            else
+            {
+                preco = 2.50;
+            }
+
+        }
+        else if(combustivel == "A")
+        {
+            if(qtde <= 20)
+            {
+                preco = 2.50;
+            }
+            else
+            {
+                preco = 1.80;
+            }
+        }
+
+        text.text = "Total = R$" + (preco * qtde).ToString();
+        	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
