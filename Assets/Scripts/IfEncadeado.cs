6a035102ab5d1f349e12860ef1c829e2a57c870c
logonrm <logonrm@fiap.com.br>
2018-03-21 11:46:59 -0300

    Aula - 21/03²

--- Assets/Scripts/IfEncadeado.cs
+++ Assets/Scripts/IfEncadeado.cs
@@ -1,19 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class IfEncadeado : MonoBehaviour {
     public Button gasolina, alcool;
-    public string combustivel;
+    string combustivel;
     double preco;
-    public  int qtde;
-    public Text text;
-	// Use this for initialization
-	void Start () {
+    int qtde;
+    public Text total,qtdeText;
+    void Start()
+    {
+        CalculaTotal();    
+    }
+    void Update()
+    {
+        total.text = "Total = R$" + (preco * qtde).ToString();
+    }
+    public void TrocarCombustivel(string tipo)
+    {
+        combustivel = tipo;
+        CalculaTotal();
+    }
+    public void AddSub(string which)
+    {
+        if (which == "Mais")
+        {
+            qtde++;
+            qtdeText.text = qtde.ToString();
+        }
+        if(which == "Menos")
+        {
+            qtde--;
+            qtdeText.text = qtde.ToString();
+        }
+        CalculaTotal();
+    }
+    public void CalculaTotal()
+    {
+        
         if (combustivel == "G")
         {
-            if(qtde <=20)
+            if (qtde <= 20)
             {
                 preco = 3;
             }
@@ -23,9 +52,9 @@ public class IfEncadeado : MonoBehaviour {
             }
 
         }
-        else if(combustivel == "A")
+        else if (combustivel == "A")
         {
-            if(qtde <= 20)
+            if (qtde <= 20)
             {
                 preco = 2.50;
             }
@@ -34,13 +63,7 @@ public class IfEncadeado : MonoBehaviour {
                 preco = 1.80;
             }
         }
-
-        text.text = "Total = R$" + (preco * qtde).ToString();
-        	}
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        qtdeText.text = qtde.ToString();
     }
+
 }
