cd9ecf4b4cedbf3a6907f29675bb34bb18bc456c
nardoritos <30866958+nardoritos@users.noreply.github.com>
2018-03-23 08:52:41 -0300

    Add files via upload

---
+++ Assets/IfEncadeadoEX21_03_18.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfEncadeadoEX21_03_18 : MonoBehaviour {
+    [SerializeField] string turno;
+    [SerializeField] int hMensais;
+    double salario;
+	// Use this for initialization
+	void Start () {
+        /*
+        Um trabalhador recebe por hora R$ 50,00, porém o mesmo reveza o turno entre dia e noite. 
+        Trabalhando no horario da noite, será acrescido 10% sobre a hora trabalhada caso o mesmo já 
+        tenha trabalhado até 10 horas no mês e no horário da manhã será acrescido 8% a partir de 
+        15º hora. Exiba o total a receber no dia trabalhado.
+        */
+        salario = 50.00;
+        if (turno == "dia")
+        {
+            if (hMensais >= 15)
+            {
+                print((salario * hMensais) + (hMensais - 15 * (salario + (salario * 0.08))));
+            }
+            else
+            {
+                print(salario * hMensais);
+            }
+        }
+        else if (turno == "noite")
+        {
+            if(hMensais >= 10)
+            {
+                print((salario * hMensais) + (hMensais - 10 * (salario + (salario * 0.10))));
+            }
+            else
+            {
+                print(salario * hMensais);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/While.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While : MonoBehaviour {
+    int numero = 0;
+	// Use this for initialization
+	void Start () {
+        //EX1 - Crie um programa que exiba os números de 0 até 150
+        #region EX1
+        /*while (numero <= 150)
+        {
+
+            print(numero);
+            numero++;
+        }*/
+        #endregion
+        //EX2 - Crie um programa que conte de 0 até 1000, exibindo apenas os números no intervalo de 150 até 160
+        #region EX2
+        while(numero <= 1000)
+        {
+            if(numero >= 150 && numero <= 160)
+            {
+                print(numero);
+                numero++;
+            }
+        }
+        #endregion  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
